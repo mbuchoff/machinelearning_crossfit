d94f3b446cf4a4944b5a9fe8d632df91d80ff088
Michael Buchoff <mbuchoff@gmail.com>
2024-05-27 18:45:56 -0400

    Split out workout_log

---
+++ workout_log.py
@@ -0,0 +1,155 @@
+from datetime import date
+
+workout_log = [
+    {
+        "date": date(2024, 5, 3),
+        "movements": [
+            {"movement": "alternating pistols", "repetitions": 20, "weight": 0},
+            {"movement": "burpees", "repetitions": 15, "weight": 0},
+            {"movement": "strict pullups", "repetitions": 10, "weight": 0},
+            {"movement": "alternating pistols", "repetitions": 20, "weight": 0},
+            {"movement": "burpees", "repetitions": 15, "weight": 0},
+            {"movement": "strict pullups", "repetitions": 10, "weight": 0},
+            {"movement": "alternating pistols", "repetitions": 20, "weight": 0},
+            {"movement": "burpees", "repetitions": 15, "weight": 0},
+            {"movement": "strict pullups", "repetitions": 10, "weight": 0},
+        ],
+        "time_minutes": 10+32/60
+    },
+    {
+        "date": date(2024, 5, 8),
+        "movements": [
+            {"movement": "running", "repetitions": 400, "weight": 0},
+            {"movement": "chest to bars", "repetitions": 10, "weight": 0},
+            {"movement": "running", "repetitions": 400, "weight": 0},
+            {"movement": "chest to bars", "repetitions": 10, "weight": 0},
+            {"movement": "dual kb front rack step-ups, 20\" box", "repetitions": 30, "weight": 53},
+            {"movement": "running", "repetitions": 25, "weight": 0},
+        ],
+        "time_minutes": 15
+    },
+    {
+        "date": date(2024, 5, 10),
+        "movements": [
+            {"movement": "power cleans", "repetitions": 7, "weight": 155},
+            {"movement": "jumping air squats", "repetitions": 30, "weight": 0},
+            {"movement": "power cleans", "repetitions": 7, "weight": 155},
+            {"movement": "jumping air squats", "repetitions": 30, "weight": 0},
+            {"movement": "power cleans", "repetitions": 7, "weight": 155},
+            {"movement": "jumping air squats", "repetitions": 30, "weight": 0},
+            {"movement": "hang power cleans", "repetitions": 7, "weight": 155},
+            {"movement": "jumping air squats", "repetitions": 30, "weight": 0},
+        ],
+        "time_minutes": 7
+    },
+    {
+        "date": date(2024, 5, 10),
+        "movements": [
+            {"movement": "power cleans", "repetitions": 7, "weight": 155},
+            {"movement": "jumping air squats", "repetitions": 30, "weight": 0},
+            {"movement": "power cleans", "repetitions": 7, "weight": 155},
+            {"movement": "jumping air squats", "repetitions": 30, "weight": 0},
+            {"movement": "power cleans", "repetitions": 7, "weight": 155},
+            {"movement": "jumping air squats", "repetitions": 30, "weight": 0},
+            {"movement": "hang power cleans", "repetitions": 7, "weight": 155},
+            {"movement": "jumping air squats", "repetitions": 30, "weight": 0},
+            {"movement": "rest", "repetitions": 2, "weight": 0},
+            {"movement": "running", "repetitions": 800, "weight": 0},
+        ],
+        "time_minutes": 12+56/60
+    },
+    {
+        "date": date(2024, 5, 13),
+        "movements": [
+            {"movement": "running", "repetitions": 200, "weight": 0},
+            {"movement": "kettlebell swings", "repetitions": 20, "weight": 70},
+            {"movement": "front rack lunges", "repetitions": 20, "weight": 95},
+            {"movement": "rest", "repetitions": 1, "weight": 95},
+            {"movement": "running", "repetitions": 200, "weight": 0},
+            {"movement": "kettlebell swings", "repetitions": 20, "weight": 70},
+            {"movement": "front rack lunges", "repetitions": 20, "weight": 95},
+            {"movement": "rest", "repetitions": 1, "weight": 95},
+            {"movement": "running", "repetitions": 200, "weight": 0},
+            {"movement": "kettlebell swings", "repetitions": 20, "weight": 70},
+            {"movement": "front rack lunges", "repetitions": 20, "weight": 95},
+            {"movement": "rest", "repetitions": 1, "weight": 95},
+            {"movement": "running", "repetitions": 200, "weight": 0},
+        ],
+        "time_minutes": 17
+    },
+    {
+        "date": date(2024, 5, 15),
+        "movements": [
+            {"movement": "running", "repetitions": 800, "weight": 0},
+            {"movement": "ring dips", "repetitions": 12, "weight": 0},
+            {"movement": "strict pullups", "repetitions": 8, "weight": 0},
+            {"movement": "ring dips", "repetitions": 12, "weight": 0},
+            {"movement": "strict pullups", "repetitions": 8, "weight": 0},
+            {"movement": "ring dips", "repetitions": 12, "weight": 0},
+            {"movement": "strict pullups", "repetitions": 8, "weight": 0},
+            {"movement": "running", "repetitions": 800, "weight": 0},
+        ],
+        "time_minutes": 12 + (51/60)
+    },
+    {
+        "date": date(2024, 5, 20),
+        "movements": [
+            {"movement": "push jerks", "repetitions": 7, "weight": 155},
+            {"movement": "front squats", "repetitions": 14, "weight": 155},
+            {"movement": "push jerks", "repetitions": 7, "weight": 155},
+            {"movement": "front squats", "repetitions": 14, "weight": 155},
+            {"movement": "push jerks", "repetitions": 7, "weight": 155},
+            {"movement": "front squats", "repetitions": 3, "weight": 155},
+        ],
+        "time_minutes": 12
+    },
+    {
+        "date": date(2024, 5, 22),
+        "movements": [
+            {"movement": "dumbbell snatch", "repetitions": 30, "weight": 50},
+            {"movement": "dumbbell walking lunges", "repetitions": 30, "weight": 50},
+            {"movement": "double unders", "repetitions": 90, "weight": 0},
+            {"movement": "dumbbell snatch", "repetitions": 30, "weight": 50},
+            {"movement": "dumbbell walking lunges", "repetitions": 30, "weight": 50},
+            {"movement": "double unders", "repetitions": 22, "weight": 0},
+        ],
+        "time_minutes": 12
+    },
+    {
+        "date": date(2024, 5, 24),
+        "movements": [
+            {"movement": "kettlebell swings", "repetitions": 30, "weight": 53},
+            {"movement": "handstand pushups", "repetitions": 12, "weight": 0},
+            {"movement": "pullups", "repetitions": 12, "weight": 0},
+            {"movement": "kettlebell swings", "repetitions": 30, "weight": 53},
+            {"movement": "handstand pushups", "repetitions": 12, "weight": 0},
+            {"movement": "pullups", "repetitions": 12, "weight": 0},
+            {"movement": "kettlebell swings", "repetitions": 30, "weight": 53},
+            {"movement": "handstand pushups", "repetitions": 7, "weight": 0},
+        ],
+        "time_minutes": 12
+    },
+    {
+        "date": date(2024, 5, 24),
+        "movements": [
+            {"movement": "running", "repetitions": 200, "weight": 0},
+            {"movement": "deadlifts", "repetitions": 22, "weight": 185},
+            {"movement": "pullups", "repetitions": 22, "weight": 0},
+            {"movement": "burpees", "repetitions": 22, "weight": 0},
+            {"movement": "kettlebell swings", "repetitions": 22, "weight": 53},
+            {"movement": "dumbbell walking lunges", "repetitions": 22, "weight": 0}, # no dumbbell
+            
+            {"movement": "running", "repetitions": 200, "weight": 0},
+            {"movement": "deadlifts", "repetitions": 22, "weight": 185},
+            {"movement": "pullups", "repetitions": 22, "weight": 0},
+            {"movement": "burpees", "repetitions": 22, "weight": 0},
+            {"movement": "kettlebell swings", "repetitions": 22, "weight": 53},
+            {"movement": "dumbbell walking lunges", "repetitions": 22, "weight": 0},
+
+            {"movement": "running", "repetitions": 200, "weight": 0},
+            {"movement": "deadlifts", "repetitions": 22, "weight": 185},
+            {"movement": "pullups", "repetitions": 5, "weight": 0},
+        ],
+        "time_minutes": 22
+    },
+]
